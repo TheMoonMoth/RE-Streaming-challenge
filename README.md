9773d5b713c2ea9015ad21363ca59841f22c996f
kweintraub <Kyle.Weintraub@dish.com>
2019-12-09 11:42:32 -0700

    basic readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Please clone the repo and follow these steps
+
+`cd video-streaming-challenge`
+`npm install`
+`npm start`
+
+Site will be available at 'localhost:5000/'
--- index.js
+++ index.js
@@ -5,28 +5,72 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(cors())
+
+app.get('/ffmpeg', (req, res) => {
+	const process = new ffmpeg('assets/tos-teaser.mp4');
+	process.then(video => {
+		console.log(video);
+		res.json(video);
+	});
+})
 
 app.get('/', (request, response) => {
   response.json({
     message: 'Welcome',
     availableRoutes: {
-      "/manifest": "A json readout of ffmpeg created manifest"
+			"/video-setup": "sample video setup step, visible as part of process",
+			"/metadata": 'Json representation of metadata and info configuration',
     }
   })
 })
 
-app.get('/video', (request, response) => {
+app.get('/video-setup', (request, response) => {
   try {
-		new ffmpeg('~/assets/tos-teaser.mp4', function (err, video) {
+		new ffmpeg('assets/tos-teaser.mp4', function (err, video) {
 			if (!err) {
 				console.log('The video is ready to be processed');
+				response.json({
+					message: 'The video is ready to be processed'
+				})
 			} else {
 				console.log('Error: ' + err);
+				response.json({
+					error: err
+				})
 			}
 		});
 	} catch (e) {
 		console.log(e.code);
 		console.log(e.msg);
+		response.json({
+			error: e
+		})
+	}
+});
+
+app.get('/metadata', (request, response) => {
+  try {
+		const process = new ffmpeg('assets/tos-teaser.mp4');
+		process.then(video => {
+			console.log("METADATA=", video.metadata);
+			console.log("CONFIG=", video.info_configuration);
+			response.json({
+				metadata: video.metadata,
+				config: video.info_configuration
+			})
+		}, error => {
+			console.log("ERROR >>> ", error);
+			response.json({
+				error
+			})
+		})
+	} catch (error) {
+		console.log(error.code);
+		console.log(error.msg);
+		response.json({
+			error
+		})
 	}
 });
 
